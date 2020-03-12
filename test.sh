6b9e9ccfffd33b5c5de795c5c94ce26be6c29836
Andrey Martyanov <andrey@martyanov.com>
2020-03-12 14:39:35 +0600

    Fix a typo

--- test.sh
+++ test.sh
@@ -5,4 +5,4 @@ set -e
 flake8 cstarmigrate
 coverage erase
 coverage run --source cstarmigrate -m py.test
-coverage report --include='cstarmigrate/**' --omit='ctarmigrate/test/**'
+coverage report --include='cstarmigrate/**' --omit='cstarmigrate/test/**'
