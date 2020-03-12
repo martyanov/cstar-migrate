a22955e55e5987568f8af08f0e2b4288daaf19c7
Andrey Martyanov <andrey@martyanov.com>
2020-03-12 15:06:11 +0600

    Move tests to a separate directory

--- cstarmigrate/__main__.py
+++ cstarmigrate/__main__.py
@@ -6,11 +6,13 @@ from __future__ import unicode_literals
 from yaml import Loader, SafeLoader
 from cstarmigrate.cli import main
 
+
 # Force YAML to load strings as unicode
 def construct_yaml_str(self, node):
-    # Override the default string handling function 
+    # Override the default string handling function
     # to always return unicode objects
     return self.construct_scalar(node)
+
 
 Loader.add_constructor('tag:yaml.org,2002:str', construct_yaml_str)
 SafeLoader.add_constructor('tag:yaml.org,2002:str', construct_yaml_str)
