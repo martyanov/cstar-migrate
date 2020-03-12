f119995d804d5df3b1811ca9e625b421746bc022
Andrey Martyanov <andrey@martyanov.com>
2020-03-12 11:36:30 +0600

    Update naming, copyrights and package meta

--- setup.py
+++ setup.py
@@ -1,23 +1,27 @@
-from setuptools import setup
+import setuptools
+
 
 VERSION = '0.3.3'
 
-setup(name='cassandra-migrate',
-      packages=['cassandra_migrate'],
-      version=VERSION,
-      description='Simple Cassandra database migration program.',
-      long_description=open('README.rst').read(),
-      url='https://github.com/Cobliteam/cassandra-migrate',
-      download_url='https://github.com/Cobliteam/cassandra-migrate/archive/{}.tar.gz'.format(VERSION),
-      author='Daniel Miranda',
-      author_email='daniel@cobli.co',
-      license='MIT',
-      install_requires=[
-          'cassandra-driver',
-          'future',
-          'PyYAML<5.0',
-          'arrow',
-          'tabulate'
-      ],
-      scripts=['bin/cassandra-migrate'],
-      keywords='cassandra schema migration')
+
+setuptools.setup(
+    name='cstar-migrate',
+    packages=['cassandra_migrate'],
+    version=VERSION,
+    description='Simple Cassandra database migration program.',
+    long_description=open('README.rst').read(),
+    url='https://github.com/martyanov/cstar-migrate',
+    download_url='https://github.com/martyanov/cstar-migrate/archive/{}.tar.gz'.format(VERSION),
+    author='Andrey Martyanov',
+    author_email='andrey@martyanov.com',
+    license='MIT',
+    install_requires=[
+        'cassandra-driver',
+        'future',
+        'PyYAML<5.0',
+        'arrow',
+        'tabulate'
+    ],
+    scripts=['bin/cassandra-migrate'],
+    keywords='cassandra schema migration',
+)
