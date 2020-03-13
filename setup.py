42ec1fed777fd475fe51c6946dd859067974d102
Andrey Martyanov <andrey@martyanov.com>
2020-03-13 15:54:01 +0600

    Reorder setup.py sections for consistency

--- setup.py
+++ setup.py
@@ -17,6 +17,21 @@ setuptools.setup(
     author_email='andrey@martyanov.com',
     license='MIT',
     license_file='LICENSE',
+    keywords='cassandra schema migration tool',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Database',
+    ],
+    project_urls={
+        'Bug Reports': 'https://github.com/martyanov/cstar-migrate/issues',
+        'Repository': 'https://github.com/martyanov/cstar-migrate',
+    },
     python_requires='>=3.7,<4.0',
     install_requires=[
         'arrow<0.16',
@@ -39,20 +54,5 @@ setuptools.setup(
         'console_scripts': [
             'cstar-migrate=cstarmigrate.__main__:main',
         ],
-    },
-    keywords='cassandra schema migration tool',
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'Intended Audience :: System Administrators',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Topic :: Database',
-    ],
-    project_urls={
-        'Bug Reports': 'https://github.com/martyanov/cstar-migrate/issues',
-        'Repository': 'https://github.com/martyanov/cstar-migrate',
     },
 )
