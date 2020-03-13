289f4b63d1abdfea714a4718d3198b296ab8c35a
Andrey Martyanov <andrey@martyanov.com>
2020-03-13 15:48:06 +0600

    Include additional package meta

--- setup.py
+++ setup.py
@@ -10,11 +10,13 @@ setuptools.setup(
     version=VERSION,
     description='Cassandra schema migration tool',
     long_description=open('README.rst').read(),
+    long_description_content_type='text/x-rst',
     url='https://github.com/martyanov/cstar-migrate',
     download_url=f'https://github.com/martyanov/cstar-migrate/archive/{VERSION}.tar.gz',
     author='Andrey Martyanov',
     author_email='andrey@martyanov.com',
     license='MIT',
+    license_file='LICENSE',
     python_requires='>=3.7,<4.0',
     install_requires=[
         'arrow<0.16',
@@ -33,10 +35,24 @@ setuptools.setup(
             'tox==3.14.5',
         ],
     },
-    keywords='cassandra schema migration',
     entry_points={
         'console_scripts': [
             'cstar-migrate=cstarmigrate.__main__:main',
         ],
+    },
+    keywords='cassandra schema migration tool',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Database',
+    ],
+    project_urls={
+        'Bug Reports': 'https://github.com/martyanov/cstar-migrate/issues',
+        'Repository': 'https://github.com/martyanov/cstar-migrate',
     },
 )
